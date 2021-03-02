247321c2977e62438e33ba8aac93467e4b4cbca4
npansuriya17 <nidhipansuriya17@gmail.com>
2021-03-02 10:00:26 -0600

    commit 2nd march

--- project4/network/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from network.models import User, Posts, Likes, Follow
-
-# Register your models here.
-
-admin.site.register(User)
-admin.site.register(Posts)
-admin.site.register(Likes)
-admin.site.register(Follow)
